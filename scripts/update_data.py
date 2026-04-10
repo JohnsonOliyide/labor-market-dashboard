a07f02f92394a9bea39f6b9bd5390c0ae1200360
JohnsonOliyide <Oliyide.ayobamij@gmail.com>
2026-04-10 18:18:53 -0500

    Update update_data.py

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -1,4 +1,9 @@
-# Source URLs
+          import requests
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
 urls = {
     "KCFed_ModelBased_Rstar_Ustar.csv": "https://kcresearch-share.kansascityfed.org/kc-mbnr/KCFed_ModelBased_Rstar_Ustar.csv",
     "UNRATE.csv": "https://fred.stlouisfed.org/graph/fredgraph.csv?id=UNRATE",
@@ -7,13 +12,13 @@ urls = {
 
 for filename, url in urls.items():
     print(f"Downloading {filename}...")
-    response = requests.get(url)
-    
+    response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         with open(filename, "wb") as f:
             f.write(response.content)
         print(f"{filename} updated.")
     else:
-        raise Exception(f"Failed to download {filename}")
+        raise Exception(f"Failed to download {filename} (status: {response.status_code})")
 
 print("All data updated successfully.")
